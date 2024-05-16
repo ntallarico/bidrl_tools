035c49e0985e621784258c83fc974fe2859b9991
ntallarico <nickdt@yahoo.com>
2024-05-16 16:20:42 -0400

    added item info update function and removal of closed items function

--- auto_bid.py
+++ auto_bid.py
@@ -43,7 +43,7 @@ def time_formatted():
     return format_2
 
 
-# scrape item objects from bidrl based on list of item_id/auction_id read in from csv.
+'''# scrape item objects from bidrl based on list of item_id/auction_id read in from csv.
 # we do this because we want the most up-to-date information for these items, like end_time_unix
 def scrape_item_objects_from_rows(browser, item_rows_list):
     print("Scraping item objects based on item_id and auction_id read from csv.")
@@ -62,11 +62,10 @@ def scrape_item_objects_from_rows(browser, item_rows_list):
         print(f"scrape_item_objects_from_rows() failed with exception: {e}")
         print("Tearing down web object.")
         browser.quit()
-        return 1
-        
+        return 1'''
 
 
-# read favorite_items_to_input_max_bid.csv and return list of item objects for items where max_desired_bid > 0
+'''# read favorite_items_to_input_max_bid.csv and return list of item objects for items where max_desired_bid > 0
 def read_user_input_csv(browser):
     try:
         filename = 'favorite_items_to_input_max_bid.csv'
@@ -78,7 +77,8 @@ def read_user_input_csv(browser):
 
         read_rows = bf.read_items_from_csv(file_path, fieldnames)
 
-        item_list = scrape_item_objects_from_rows(browser, read_rows)
+        #item_list = scrape_item_objects_from_rows(browser, read_rows)
+        item_list = create_item_objects_from_rows(read_rows)
 
         items_to_bid_on = []
         item_count_with_desired_bid = 0
@@ -111,8 +111,117 @@ def read_user_input_csv(browser):
         print(f"read_user_input_csv() failed with exception: {e}")
         print("Tearing down web object.")
         browser.quit()
-        return 1
+        return 1'''
     
+
+# updates select item info in a list of item objects
+# requires: webdriver object, list of item objects
+# returns: nothing
+# changes: item objects in list
+def update_item_info(browser, items):
+    try:
+        print("Updating item data from bidrl.")
+
+        # get distinct list of auction_ids from items list
+        auction_ids = []
+        for item in items:
+            if item.auction_id not in auction_ids:
+                auction_ids.append(item.auction_id)
+
+        new_items = []
+        # fast scrape all items from all auctions in list
+        for auction_id in auction_ids:
+            scraped_items = bf.scrape_items_fast(browser, auction_id, get_images = 'false')
+            new_items.extend(scraped_items) # union lists together into new_items
+        
+        # update fields in items list
+        for item in items:
+            for new_item in new_items:
+                if new_item.id == item.id:
+                    item.end_time_unix = new_item.end_time_unix
+                    item.highbidder_username = new_item.highbidder_username
+
+        print("Success.")
+        return 0
+    
+    except Exception as e:
+        print(f"update_item_info() failed with exception: {e}")
+        return 1
+
+
+def create_item_objects_from_rows(item_rows_list):
+    item_list = [] # list for item objects to return at the end
+    for item_row in item_rows_list:
+        # check if max_desired_bid is not empty. if it isn't, then convert to float. if it is, then set to None
+        max_desired_bid = float(item_row['max_desired_bid']) if item_row['max_desired_bid'] != '' else None
+
+        # extract data from json into temp dictionary to create item with later
+        temp_item_dict = {'id': item_row['item_id']
+                                , 'auction_id': item_row['auction_id']
+                                , 'description': item_row['description']
+                                , 'url': item_row['url']
+                                , 'end_time_unix': int(item_row['end_time_unix'])
+                                , 'max_desired_bid': max_desired_bid}
+        
+        item_list.append(Item(**temp_item_dict))
+    return item_list
+        
+# read favorite_items_to_input_max_bid.csv and return list of item objects for items where max_desired_bid > 0
+def read_user_input_csv_to_item_objects(browser):
+    try:
+        filename = 'favorite_items_to_input_max_bid.csv'
+        path_to_file = 'local_files/'
+
+        file_path = path_to_file + filename
+
+        fieldnames = ['end_time_unix', 'auction_id', 'item_id', 'description', 'max_desired_bid', 'url']
+
+        read_rows = bf.read_items_from_csv(file_path, fieldnames)
+
+        item_list = create_item_objects_from_rows(read_rows)
+
+        items_to_bid_on = []
+        item_count_with_desired_bid = 0
+        item_count_zero_desired_bid = 0
+        item_count_no_desired_bid = 0
+        for item in item_list:
+            if item.max_desired_bid == 0:
+                item_count_zero_desired_bid += 1
+            elif item.max_desired_bid == None:
+                item_count_no_desired_bid += 1
+            else:
+                item_count_with_desired_bid += 1
+                items_to_bid_on.append(item)
+
+        print(f"\nRead file: {filename}.")
+        print(f"Items with max_desired_bid: {item_count_with_desired_bid}")
+        print(f"Items without max_desired_bid: {item_count_no_desired_bid}")
+        print(f"Items with 0 max_desired_bid: {item_count_zero_desired_bid}\n")
+
+        # sort list of items in descending order based on their end time
+        items_to_bid_on.sort(key=lambda x: x.end_time_unix, reverse=True)
+
+        return items_to_bid_on
+    except Exception as e:
+        print(f"read_user_input_csv() failed with exception: {e}")
+        print("Tearing down web object.")
+        browser.quit()
+        return 1
+
+
+# deleted any item object from items_to_bid_on that has already passed its close time
+def remove_closed_items(items_to_bid_on):
+    print("\nRemoving items that are already closed.")
+    closed_items = 0
+    current_time_unix = time_unix()
+    # loop through each item and bid on it if the time remaining on the item is <= our set seconds_before_closing_to_bid time
+    for item in items_to_bid_on[:]:
+        remaining_seconds = item.end_time_unix - current_time_unix
+        if remaining_seconds < 0:
+            closed_items += 1
+            items_to_bid_on.remove(item)
+    print(f"Closed items removed: {closed_items}")
+    return 0
 
 
 def print_items_status(items_to_bid_on):
@@ -128,9 +237,9 @@ def print_items_status(items_to_bid_on):
 
 
 def auto_bid(browser, items_to_bid_on, seconds_before_closing_to_bid):
-    current_time_unix = int(time.time())
+    current_time_unix = time_unix()
     # loop through each item and bid on it if the time remaining on the item is <= our set seconds_before_closing_to_bid time
-    for item in items_to_bid_on:
+    for item in items_to_bid_on[:]:
         remaining_seconds = item.end_time_unix - current_time_unix
         if remaining_seconds <= seconds_before_closing_to_bid:
             bf.bid_on_item(item, item.max_desired_bid, browser)
@@ -152,34 +261,46 @@ def login_refresh(browser, last_login_time, last_login_time_unix):
         print(f"Login check success. Last login: {last_login_time}. Time remained logged in: {logged_in_time}.")
 
 
-def auto_bid_main(seconds_before_closing_to_bid # add 5 secs to account for POST time to API. don't want to extend bid time if we can avoid
-         , auto_bid__interval # how often to check if it is time to bid on each item (and then bid if it is)
-         , print_items_status__interval # how often to print times remaining for all items
-         , login_refresh__interval # we want to keep this reasonable because it actually submits a request to bidrl
+def auto_bid_main(seconds_before_closing_to_bid = 120 + 5 # add 5 secs to account for POST time to API. don't want to extend bid time if we can avoid
+         , auto_bid__interval = 5 # how often to check if it is time to bid on each item (and then bid if it is)
+         , print_items_status__interval = 60 # how often to print times remaining for all items
+         , login_refresh__interval = 60 # keep this reasonable - actually submits a request to bidrl
+         , update_item_info__interval = 60 * 60 # keep this reasonable - actually submits a request to bidrl
          ):
     
     # get an initialized web driver that has logged in to bidrl with credentials stored in config.py
     browser = bf.get_logged_in_webdriver(user_email, user_password, 'headless')
+
+    # TODO get user session and set variable username (so we can later check if highbidder_username = username)
+
     last_login_time_string = time_formatted()
     last_login_time_unix = time_unix()
 
     # read favorite_items_to_input_max_bid.csv and return list of item objects we intend to bid on
-    items_to_bid_on = read_user_input_csv(browser)
+    items_to_bid_on = read_user_input_csv_to_item_objects(browser)
+
+    update_item_info(browser, items_to_bid_on)
+
+    remove_closed_items(items_to_bid_on)
 
     print(f"\nWe intend to bid on {len(items_to_bid_on)} items, {seconds_before_closing_to_bid} seconds before they close, checking every {auto_bid__interval} seconds.\n")
     
-    
-
     # set x__last_run_time to 0 to run immediately when loop processes
     # set to time_unix() to wait x__interval amount of seconds first
     auto_bid__last_run_time = 0
     login_refresh__last_run_time = 0
     print_items_status__last_run_time = 0
+    update_item_info__last_run_time = time_unix()
 
     try:
         # loop until KeyboardInterrupt, testing if it has been x_function__interval seconds since last run of x_function()
         # if it has been, run x_function(), then test the next function and its corresponding times
         while True:
+            # check if time to run: update_item_info()
+            if time_unix() - update_item_info__last_run_time >= update_item_info__interval:
+                update_item_info(browser, items_to_bid_on)
+                update_item_info__last_run_time = time_unix()
+
             # check if time to run: login_refresh()
             if time_unix() - login_refresh__last_run_time >= login_refresh__interval:
                 login_refresh(browser, last_login_time_string, last_login_time_unix)
@@ -202,10 +323,22 @@ def auto_bid_main(seconds_before_closing_to_bid # add 5 secs to account for POST
 
 
 if __name__ == "__main__":
-  auto_bid_main(seconds_before_closing_to_bid = 120 + 5
-          , auto_bid__interval = 5
-          , print_items_status__interval = 60
-          , login_refresh__interval = 60)
+  auto_bid_main()
+
+
+'''
+what we need to to:
+- after reading in csv, parse out two lists, Items, and Item Groups
+- print out how many items and item groups we have
+- create a second function auto_bid_item_group that works on item groups
+    - this will call scrape_items_fast and update our wins and times and whatever
+- set up item group shit in csv creation
+- have scrape_open_auctions_to_csv.py scrape to sql database instead of csv
+- update create_user_input_csv.py to read from sql database
+- update create_user_input_csv.py to output to google sheet (leave option in code for using csv still? - in case anyone wants to use git repo)
+- update auto_bid.py to read from google sheet, re-reading every hour or whatever
+
+'''
 
 
 
