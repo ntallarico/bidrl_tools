c6c02e1bde24576f0ab2212c6f265b2b3027e807
ntallarico <nickdt@yahoo.com>
2024-10-20 16:42:20 -0400

    add urls as tasks to todoist

--- generate_auction_link_list.py
+++ generate_auction_link_list.py
@@ -1,11 +1,14 @@
 '''
 This script generates a list of auction links, each set to show the maximum items per page.
+If todoist_api_token is defined in config.py, then add urls as tasks in Todoist.
 '''
 
 import time
-from datetime import datetime
+from datetime import datetime, date
 import bidrl_functions as bf
 from config import home_affiliates
+from todoist_api_python.api import TodoistAPI
+import json
 
 
 def generate_auction_link_list():
@@ -22,6 +25,39 @@ def generate_auction_link_list():
     for url in auction_urls:
         print(url)
     print("")
+
+    # check if todoist api is defined in config file, and if it is, proceed with adding urls as tasks in todoist
+    try:
+        from config import todoist_api_token
+    except ImportError:
+        todoist_api_token = None
+
+    if not todoist_api_token:
+        #print("\ntodoist_api_token is not defined in config.py. Todoist integration.")
+        return
+    else:
+        print("\ntodoist_api_token found in config.py. Proceeding")
+        print("Attempting to post URLs to Todoist")
+
+        parent_task_name = 'Review BidRL auctions for this week'
+
+        # Initialize the Todoist API
+        api = TodoistAPI(todoist_api_token)
+
+        # Create the parent task
+        try:
+            parent_task = api.add_task(content=parent_task_name, due_string='today')
+            print(f"Parent task created: {parent_task.content}")
+        except Exception as error:
+            print(f"Error creating parent task: {error}")
+
+        # Create subtasks for each auction URL
+        for url in auction_urls:
+            try:
+                subtask = api.add_task(content=url, parent_id=parent_task.id)
+                print(f"Subtask task created (parent ID {parent_task.id}): [...]{subtask.content[-40:]}")
+            except Exception as error:
+                print(f"Error creating subtask for {url}: {error}")
 
 
 if __name__ == "__main__":
