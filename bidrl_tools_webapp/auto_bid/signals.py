985ea2e926157b1977bd6ff4be1510c4442b6122
ntallarico <nickdt@yahoo.com>
2024-11-22 16:50:54 -0500

    commented out channels attempt

--- bidrl_tools_webapp/auto_bid/signals.py
+++ bidrl_tools_webapp/auto_bid/signals.py
@@ -5,26 +5,26 @@ from .models import ItemUserInput
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
 
-@receiver(post_save, sender=ItemUserInput)
-def notify_item_update(sender, instance, created, **kwargs):
-    # This function will be called after an ItemUserInput instance is saved
-    print(f"Item updated: {instance.id}")  # Debugging line to ensure the signal is triggered
+# @receiver(post_save, sender=ItemUserInput)
+# def notify_item_update(sender, instance, created, **kwargs):
+#     # This function will be called after an ItemUserInput instance is saved
+#     print(f"Item updated: {instance.item_id}")  # Debugging line to ensure the signal is triggered
 
-    # Get the channel layer
-    channel_layer = get_channel_layer()
+#     # Get the channel layer
+#     channel_layer = get_channel_layer()
 
-    # Send a message to the WebSocket group
-    async_to_sync(channel_layer.group_send)(
-        "open_items_group",  # Group name
-        {
-            "type": "item_update",
-            "message": {
-                "description": instance.description,
-                "max_desired_bid": instance.max_desired_bid,
-                "item_bid_group_id": instance.item_bid_group_id,
-                "ibg_items_to_win": instance.ibg_items_to_win,
-                "current_bid": instance.current_bid,
-                "highbidder_username": instance.highbidder_username,
-            }
-        }
-    )
+#     # Send a message to the WebSocket group
+#     async_to_sync(channel_layer.group_send)(
+#         "open_items_group",  # Group name
+#         {
+#             "type": "item_update",
+#             "message": {
+#                 "description": instance.description,
+#                 "max_desired_bid": instance.max_desired_bid,
+#                 "item_bid_group_id": instance.item_bid_group_id,
+#                 "ibg_items_to_win": instance.ibg_items_to_win,
+#                 "current_bid": instance.current_bid,
+#                 "highbidder_username": instance.highbidder_username,
+#             }
+#         }
+#     )
--- bidrl_tools_webapp/bidrl_tools_webapp/settings.py
+++ bidrl_tools_webapp/bidrl_tools_webapp/settings.py
@@ -138,7 +138,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-
 """
 what do we need
 
