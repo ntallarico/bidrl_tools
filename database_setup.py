cf8e2bb867ce3890620056c2854515ce531cc119
ntallarico <nickdt@yahoo.com>
2025-05-26 15:25:38 -0400

    v_reporting_user additions

--- database_setup.py
+++ database_setup.py
@@ -14,57 +14,6 @@ def drop_database(conn):
     print("All tables dropped successfully.")
 
 
-# create user reporting view. drop it if it already exists first
-def create_v_reporting_user(conn):
-    bf.drop_and_create_view(conn, 'v_reporting_user', '''
-        CREATE VIEW v_reporting_user AS
-
-        WITH usernames as (
-            SELECT DISTINCT
-                username
-            FROM bids
-        ), bid_info as (
-            SELECT
-                username
-                , COUNT(DISTINCT b.bid_id) AS bids_placed
-                , MIN(b.time_of_bid_unix) AS earliest_bid_time_unix
-                , MAX(b.time_of_bid_unix) AS latest_bid_time_unix
-                , COUNT(DISTINCT b.item_id) AS items_bid_on
-            FROM bids b
-            GROUP BY
-                username
-        )
-        SELECT
-            u.username
-            , af.company_name
-            , ROUND(SUM(i.current_bid), 2) AS total_bid
-            , ROUND(SUM(i.total_cost), 2) AS total_spent
-            , COUNT(DISTINCT i.item_id) AS items_bought
-            , MAX(i.current_bid) AS most_expensive_purchase
-            , COUNT(DISTINCT IIF(i.total_cost < 2, i.item_id, NULL)) AS purchase_count_total_cost_0_to_1_99 -- count of purchases with actual cost of >$2
-            , COUNT(DISTINCT IIF(i.total_cost < 5, i.item_id, NULL)) AS purchase_count_total_cost_2_to_4_99 -- count of purchases with actual cost of >$5
-            , COUNT(DISTINCT IIF(i.current_bid < 2, i.item_id, NULL)) AS purchase_count_bid_0_to_1_99 -- count of purchases with bid amount of >$2
-            , COUNT(DISTINCT IIF(i.current_bid < 5, i.item_id, NULL)) AS purchase_count_bid_2_to_4_99 -- count of purchases with bid amount of >$5
-            , bi.bids_placed
-            , bi.earliest_bid_time_unix
-            , bi.latest_bid_time_unix
-            , bi.items_bid_on
-            --, '' AS most_bought_category
-            --, '' AS most_spent_category
-            --, '' AS closest_snipe
-            --, '' AS longest_out_bid_that_won
-        FROM usernames u
-            LEFT JOIN items i on i.highbidder_username = u.username -- specifically join on items where the user won
-            LEFT JOIN auctions au on au.auction_id = i.auction_id
-            LEFT JOIN affiliates af on af.affiliate_id = au.affiliate_id
-            LEFT JOIN bid_info bi on bi.username = u.username
-        WHERE au.status = 'closed'
-        GROUP BY
-            u.username
-            , af.company_name
-        ;
-    ''')
-
 # create bid info view. drop it if it already exists first
 def create_v_bid_info(conn):
     bf.drop_and_create_view(conn, 'v_bid_info', '''
@@ -87,6 +36,60 @@ def create_v_bid_info(conn):
                 ELSE 'Unmapped'
                 END AS bid_type
         FROM bid_flags bf
+        ;
+    ''')
+
+# create user reporting view. drop it if it already exists first
+def create_v_reporting_user(conn):
+    bf.drop_and_create_view(conn, 'v_reporting_user', '''
+        CREATE VIEW v_reporting_user AS
+
+        WITH usernames as (
+            SELECT DISTINCT
+                username
+            FROM bids
+        ), bid_info as (
+            SELECT
+                username
+                , COUNT(DISTINCT b.bid_id) AS bids_placed
+                , COUNT(DISTINCT (CASE WHEN bi.bid_type = 'Manual' THEN b.bid_id ELSE NULL END)) AS manual_bids_placed
+                , MIN(b.time_of_bid_unix) AS earliest_bid_time_unix
+                , MAX(b.time_of_bid_unix) AS latest_bid_time_unix
+                , COUNT(DISTINCT b.item_id) AS items_bid_on
+            FROM bids b
+                LEFT JOIN v_bid_info bi ON bi.bid_id = b.bid_id
+            GROUP BY
+                username
+        )
+        SELECT
+            u.username
+            , af.company_name
+            , ROUND(SUM(i.current_bid), 2) AS total_bid
+            , ROUND(SUM(i.total_cost), 2) AS total_spent
+            , COUNT(DISTINCT i.item_id) AS items_bought
+            , MAX(i.current_bid) AS most_expensive_purchase
+            , COUNT(DISTINCT IIF(i.total_cost < 2, i.item_id, NULL)) AS purchase_count_total_cost_0_to_1_99 -- count of purchases with actual cost of >$2
+            , COUNT(DISTINCT IIF(i.total_cost < 5, i.item_id, NULL)) AS purchase_count_total_cost_2_to_4_99 -- count of purchases with actual cost of >$5
+            , COUNT(DISTINCT IIF(i.current_bid < 2, i.item_id, NULL)) AS purchase_count_bid_0_to_1_99 -- count of purchases with bid amount of >$2
+            , COUNT(DISTINCT IIF(i.current_bid < 5, i.item_id, NULL)) AS purchase_count_bid_2_to_4_99 -- count of purchases with bid amount of >$5
+            , bi.bids_placed
+            , bi.manual_bids_placed
+            , bi.earliest_bid_time_unix
+            , bi.latest_bid_time_unix
+            , bi.items_bid_on
+            --, '' AS most_bought_category
+            --, '' AS most_spent_category
+            --, '' AS closest_snipe
+            --, '' AS longest_out_bid_that_won
+        FROM usernames u
+            LEFT JOIN items i ON i.highbidder_username = u.username -- specifically join on items where the user won
+            LEFT JOIN auctions au ON au.auction_id = i.auction_id
+            LEFT JOIN affiliates af ON af.affiliate_id = au.affiliate_id
+            LEFT JOIN bid_info bi ON bi.username = u.username
+        WHERE au.status = 'closed'
+        GROUP BY
+            u.username
+            , af.company_name
         ;
     ''')
 
@@ -221,11 +224,11 @@ def sql_database_setup():
 
     print('')
 
-    # create view v_reporting_user
-    create_v_reporting_user(conn)
-
     # create view v_bid_info
     create_v_bid_info(conn)
+
+    # create view v_reporting_user
+    create_v_reporting_user(conn)
 
 
     ##### indexes #####
